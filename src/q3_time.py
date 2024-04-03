cd8ced20983ec7fad13ec0276249473106a1cfaf
VicenteOlmos <71791058+VicenteOlmos@users.noreply.github.com>
2024-04-03 11:13:45 -0300

    BST menciones y BBDD menciones
    - Se hace el BST para menciones
    - Se crea la BBDD para menciones
    - Calculo de memoria y tiempo de ejecucion

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,37 @@
+import json
+import re
+import sqlite3
 from typing import List, Tuple
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    conn = sqlite3.connect("mentions.db")
+    c = conn.cursor()
+
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS mentions
+                 (user TEXT)"""
+    )
+    conn.commit()
+
+    c.execute("SELECT COUNT(*) FROM mentions")
+    count = c.fetchone()[0]
+    if count == 0:
+        with open(file_path, "r") as file:
+            for line in file:
+                tweet = json.loads(line)
+                tweet_text = tweet["content"]
+                mentions = re.findall(r"@(\w+)", tweet_text)
+                for mention in mentions:
+                    c.execute(
+                        "INSERT INTO mentions (user) VALUES (?)",
+                        (mention,),
+                    )
+        conn.commit()
+
+    query_most_mentioned = """ SELECT user,COUNT(*) as counter FROM mentions GROUP BY user ORDER BY counter DESC LIMIT 10"""
+    c.execute(query_most_mentioned)
+
+    most_mentioned = c.fetchall()
+    conn.close()
+        
+    return most_mentioned
