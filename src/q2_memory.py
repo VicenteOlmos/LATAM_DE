66360f163a56abdcd70fa16ef83aa2f9b17be2f0
VicenteOlmos <71791058+VicenteOlmos@users.noreply.github.com>
2024-04-03 10:52:07 -0300

    Tiempos de ejecucion, uso de memoria, BBDD emoji
    - Se añaden tiempos de ejecucion y uso de memoria para la q2
    - Se arregla error al insertar los tweets en la BBDD
    - Se añaden las librerias usadas

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,49 @@
+import json
 from typing import List, Tuple
+import emoji
+
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    class BSTNode:
+        def __init__(self, emoji):
+            self.emoji = emoji
+            self.counter = 1
+            self.left = None
+            self.right = None
+
+        def insert(self, emoji):
+            if emoji < self.emoji:
+                if self.left is None:
+                    self.left = BSTNode(emoji)
+                else:
+                    self.left.insert(emoji)
+            elif emoji > self.emoji:
+                if self.right is None:
+                    self.right = BSTNode(emoji)
+                else:
+                    self.right.insert(emoji)
+            else:
+                self.counter += 1
+
+        def get_top_emojis(self):
+            data = []
+            if self.left:
+                data.extend(self.left.get_top_emojis())
+            data.append((self.emoji, self.counter))
+            if self.right:
+                data.extend(self.right.get_top_emojis())
+            return data
+
+    root = None
+    with open(file_path, "r") as file:
+        for line in file:
+            tweet = json.loads(line)
+            for char in tweet["content"]:
+                if char in emoji.UNICODE_EMOJI["en"]:
+                    if root is None:
+                        root = BSTNode(char)
+                    else:
+                        root.insert(char)
+
+    all_data = root.get_top_emojis() if root else []
+    return sorted(all_data, key=lambda x: x[1], reverse=True)[:10]
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,44 @@
+import json
+import sqlite3
 from typing import List, Tuple
+import emoji
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    conn = sqlite3.connect("emojis.db")
+    c = conn.cursor()
+
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS tweets
+                 (content TEXT)"""
+    )
+    conn.commit()
+
+    c.execute("SELECT COUNT(*) FROM tweets")
+    count = c.fetchone()[0]
+    if count == 0:
+        with open(file_path, "r") as file:
+            for line in file:
+                tweet = json.loads(line)
+                tweet_text = tweet["content"]
+                c.execute(
+                    "INSERT INTO tweets (content) VALUES (?)",
+                    (str(tweet_text),),
+                )
+        conn.commit()
+
+    query_top_emojis = """ SELECT content FROM tweets"""
+    c.execute(query_top_emojis)
+
+    emoji_counter = {}
+    for row in c.fetchall():
+        tweet = row[0]
+        for char in tweet:
+            if char in emoji.UNICODE_EMOJI["en"]:
+                if char in emoji_counter:
+                    emoji_counter[char] += 1
+                else:
+                    emoji_counter[char] = 1
+    conn.close()
+
+    return sorted(emoji_counter.items(), key=lambda x: x[1], reverse=True)[:10]
